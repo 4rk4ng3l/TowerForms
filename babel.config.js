c28124942c952f0fcc515ca9428847a76a13838c
AlexFonsek <german.fonseca33@gmail.com>
2026-01-09 05:46:02 -0500

    Correcion bug babel

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,22 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        alias: {
-          '@core': './src/core',
-          '@data': './src/data',
-          '@infrastructure': './src/infrastructure',
-          '@presentation': './src/presentation',
-          '@store': './src/store',
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./app/src'],
+          alias: {
+            '@': './',
+            '@core': './app/src/core',
+            '@data': './app/src/data',
+            '@infrastructure': './app/src/infrastructure',
+            '@presentation': './app/src/presentation',
+            '@store': './app/src/store',
+          },
         },
-      },
+      ],
     ],
-  ],
+  };
 };
